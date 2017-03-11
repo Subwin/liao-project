5c28cf7f85f32cbfdb84336cfa65428336a26451
Subwin <subwin_zombie@163.com>
2017-03-11 19:35:52 +0800

    修改‘

--- message.py
+++ message.py
@@ -1,72 +1,106 @@
-import requests
-import json
-import http
+import re
 import urllib
+import requests
+from lxml import html
 
 # 登录的主页面
-HOSTURL = 'http://10.72.66.180'
-# post数据接收和处理的页面（我们要向这个页面发送我们构造的Post数据）
-LOGINURL = 'http://10.72.66.180/rone/login'
-
-SEND_MASSAGE_API = "http://10.72.66.180/affairs/affSMSApply/saveNotice"
-USER_ID = 'liaozqhh'
-PASSWORD = 'yx123456lzq'
+HOST = 'http://10.72.66.180'
+LOGIN = '/rone/login'
+ADD_MESSAGE = '/affairs/affSMSApply/addNotice'
+SEND_MASSAGE = '/affairs/affSMSApply/saveNotice'
 
 
-# TODO 接口细化，从网页抓取用户信息填入data
-def gen_post_data():
-    paramsJson = {
-        "applyDeptCode": "01243120306",
-        "applyDeptName": "市场服务分部",
-        "applyUserName": "廖志强",
-        "applyUserCode": "4214312011793",
-        "topOrgCode": "012431203",
-        "smsContent": "接口问题搞定！",
-    }
 
-    contactJson = {
-        "receiveUserCode": "",
-        "receiveUserName": "",
-        "receiveUserPhone": "15607444616",
-        "smsScheduler": "",
-        "sendMessageValue": "0"
-    }
+class ParamsJson(object):
+    applyDeptCode = ''
+    applyDeptName = ''
+    applyUserName = ''
+    applyUserCode = ''
+    topOrgCode = ''
+    smsContent = ''
 
-    data = dict(
-        paramsJson=paramsJson,
-        contactJson=contactJson
-    )
+    def get_params(self, page):
+        # print(page.text)
+        tree = html.fromstring(page.text)
+        parsed_tree = tree.xpath('//*[@id="divSelGroup"]/table/tr/td/input')
 
-    return urllib.parse.urlencode(data)
+        # 短信页面拿以下的参数
+        self.applyDeptCode = parsed_tree[0].value
+        self.applyDeptName = parsed_tree[1].value
+        self.applyUserName = parsed_tree[2].value
+        self.applyUserCode = parsed_tree[3].value
+        self.topOrgCode = parsed_tree[5].value
 
 
-def send_message(session):
-    headers = {"Content-Type": "application/x-www-form-urlencoded"}
-    postData = gen_post_data()
-    r = session.post(SEND_MASSAGE_API, data=postData, headers=headers)
-    print(r.content.decode())
+class ContactJson(object):
+    def __init__(self):
+        self.receiveUserCode = ''
+        self.receiveUserName = ''
+        self.receiveUserPhone = ''
+        self.smsScheduler = ''
+        self.sendMessageValue = '0'
 
 
-def login(username, password):
-    username = 'liaozqhh'
-    password = 'yx123456lzq'
-    data = dict(
-        userid=username,
-        userName=username,
-        j_username=username,
-        password=password,
-        j_password=password,
-    )
+class AddNoticeParams(object):
+    def __init__(self):
+        self.result = 'login'
+        self.menuType = 3
+        self.mgId = 37
+        self.menuId = 121
+        self.resuuid = '8ac89d6039dc2e600139dc2f39cc0001'
+        self.LTPAToken = ''
+        self.appId = '8ac8c23239d246830139d3f531770008'
 
-    login_url = 'http://10.72.66.180/rone/login'
-    sss = requests.Session()
-    sss.post(login_url, data=data)
 
-    return sss
+class LoginWorker(object):
+    def __init__(self):
+        self.userid = ''
+        self.userName = ''
+        self.j_username = ''
+        self.password = ''
+        self.j_password = ''
+
+    def login(self, session, username, password):
+        self.userid = username
+        self.userName = username
+        self.j_username = username
+        self.password = password
+        self.j_password = password
+
+        page = session.post(HOST + LOGIN, data=self.__dict__)
+        m = re.search(r'LTPAToken=(.*)"', page.text)
+        return m.group(1)
+
+
+class SendMessageWorker(object):
+    def __init__(self, username, password):
+        self.params_json = ParamsJson()
+        self.contact_json = ContactJson()
+        self.add_notice_params = AddNoticeParams()
+        self.login_params = LoginWorker()
+        self.session = requests.Session()
+
+        # 从首页取得LTPAToken
+        token = self.login_params.login(session=self.session, username=username, password=password)
+        self.add_notice_params.LTPAToken = token
+
+        # 从短信页面取得发短信的所需字段
+        page = self.session.get(HOST + ADD_MESSAGE, params=self.add_notice_params.__dict__)
+        self.params_json.get_params(page)
+
+
+    def send_message(self, sms_content, receiver_phone):
+        self.params_json.smsContent = sms_content
+        self.contact_json.receiveUserPhone = receiver_phone
+        data = dict(
+            paramsJson=self.params_json.__dict__,
+            contactJson=self.contact_json.__dict__
+        )
+        data = urllib.parse.urlencode(data)
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self.session.post(HOST+SEND_MASSAGE, data=data, headers=headers)
 
 
 if __name__ == "__main__":
-    session = login(username=USER_ID, password=PASSWORD)
-    send_message(session)
-
-
+    worker = SendMessageWorker('liaozqhh', 'yx123456lzq')
+    worker.send_message('hello world', '15607444616')
